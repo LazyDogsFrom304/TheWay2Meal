b1813347c1242d244dff6956b65df371016dc73e
leonardozcm <597714572@qq.com>
2021-12-03 11:35:04 +0800

    meal html done

--- controller/menuctl.go
+++ controller/menuctl.go
@@ -17,9 +17,8 @@ func floorSelector(floor int) func(interface{}) bool {
 }
 
 func menuHandler(c *gin.Context) {
-	//解析模板文件
+
 	t := template.Must(template.ParseFiles(HTMLPath + "Menu.html"))
-	//声明一个字符串切片
 
 	_firstFloor := service.MealService.Select(0, floorSelector(1))
 	_secondFloor := service.MealService.Select(0, floorSelector(2))
@@ -27,8 +26,5 @@ func menuHandler(c *gin.Context) {
 	fmt.Println(_secondFloor...)
 	_floorList := [...]interface{}{_firstFloor, _secondFloor}
 
-	// stars := []string{"马蓉", "李小璐", "白百何"}
-
-	//执行模板
 	t.Execute(c.Writer, _floorList)
 }
